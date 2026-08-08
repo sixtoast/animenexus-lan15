b2057d6c97bb1e33fc426f5aece0c3f0584b5c6f
sixtoast <segoramasodi16@gmail.com>
2026-08-08 12:36:05 +0200

    Align stack: React 19 + R3F 9 + Next 15.5; remove React 18 pin that broke cache()

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,4 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
@@ -10,17 +9,6 @@ const nextConfig: NextConfig = {
       { protocol: "https", hostname: "cdn.myanimelist.net" },
       { protocol: "https", hostname: "placehold.co" },
     ],
-  },
-  // Force a single React instance so R3F does not see a second copy
-  // (fixes: undefined is not an object (evaluating '…ReactCurrentBatchConfig'))
-  webpack: (config) => {
-    config.resolve = config.resolve || {};
-    config.resolve.alias = {
-      ...(config.resolve.alias || {}),
-      react: path.resolve("./node_modules/react"),
-      "react-dom": path.resolve("./node_modules/react-dom"),
-    };
-    return config;
   },
 };
 
