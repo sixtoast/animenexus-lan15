0309e753d5a0a6e4313bf8fa1b05051dc1d75d38
sixtoast <segoramasodi16@gmail.com>
2026-08-17 20:00:12 +0200

    feat(sprint1): NexusRouteBeacon in root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,7 @@ import { RouteTune } from "@/components/RouteTune";
 import { LanternMemoryBoot } from "@/components/LanternMemoryBoot";
 import { SealMomentHost } from "@/components/SealMoment";
 import { EnvironmentController } from "@/components/EnvironmentController";
+import { NexusRouteBeacon } from "@/components/NexusRouteBeacon";
 import { MascotHost } from "@/components/mascot/MascotHost";
 import { MascotErrorBoundary } from "@/components/mascot/MascotErrorBoundary";
 import "./globals.css";
@@ -94,6 +95,7 @@ export default function RootLayout({
               <WatchlistProvider>
                 <SessionProvider>
                   <LanternMemoryBoot />
+                  <NexusRouteBeacon />
                   <EnvironmentController />
                   <ScrollProgress />
                   <SakuraCanvas />
