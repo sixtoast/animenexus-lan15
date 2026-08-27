bd173183d912c020e707347855ce1e8468a091df
sixtoast <segoramasodi16@gmail.com>
2026-08-27 19:42:22 +0200

    feat(creative-2): rive barrel export

---
+++ components/rive/index.ts
@@ -0,0 +1,9 @@
+export { NexusRive, type NexusRiveProps } from "./NexusRive";
+export { RiveFallback, type RiveFallbackProps } from "./RiveFallback";
+export {
+  applyRiveState,
+  stateFromAsync,
+  RIVE_INPUT_STATES,
+  type RiveInputState,
+  type RiveBridgeTarget,
+} from "./RiveStateBridge";
