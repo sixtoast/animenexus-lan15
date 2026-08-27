0496fd6fddba85493c128b248e5c749356065f77
sixtoast <segoramasodi16@gmail.com>
2026-08-27 19:44:25 +0200

    feat(creative-3): export state hooks and shell

--- components/rive/index.ts
+++ components/rive/index.ts
@@ -2,8 +2,15 @@ export { NexusRive, type NexusRiveProps } from "./NexusRive";
 export { RiveFallback, type RiveFallbackProps } from "./RiveFallback";
 export {
   applyRiveState,
+  applyRiveBand,
+  resolveRiveState,
   stateFromAsync,
   RIVE_INPUT_STATES,
   type RiveInputState,
   type RiveBridgeTarget,
+  type AsyncVisualStatus,
+  type PointerVisualStatus,
+  type RiveInstrumentBand,
 } from "./RiveStateBridge";
+export { useRiveAppState, type UseRiveAppStateOptions } from "./useRiveAppState";
+export { RiveButtonShell, type RiveButtonShellProps } from "./RiveButtonShell";
