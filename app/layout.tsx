247db2a3dcb39919618afcaaf9fefb4b9eeea1b9
sixtoast <segoramasodi16@gmail.com>
2026-09-25 22:50:02 +0200

    fix(ui): correct cinema stylesheet import

--- app/layout.tsx
+++ app/layout.tsx
@@ -102,7 +102,8 @@ import "./nexus-icon.css";
 import "./nexus-icon-optical.css";
 import "./rive.css";
 import "./tool-identity.css";
-import "./restraint.css";\nimport "./cinema-ui.css";
+import "./restraint.css";
+import "./cinema-ui.css";
 
 export const metadata: Metadata = {
   title: "AnimeNexus \u2014 Lantern",
